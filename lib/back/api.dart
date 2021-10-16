ec4a5a51c90d7de6b29c044b49de3f2d963baee9
Raif <50047969+raifpy@users.noreply.github.com>
2021-10-16 12:38:47 +0300

    Changed aom subdomain to 'depo'

--- lib/back/api.dart
+++ lib/back/api.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:coderatorfoss/back/template.dart';
 
-String apiURL = "https://aom.coderator.net";
+String apiURL = "https://depo.coderator.net";
 
 class ApiRequest {
   String path;
